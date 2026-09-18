58cdf046ff6b51091741bfed03e9b5db5e88b143
Kartik Varshney <kartikvarshney1978@gmail.com>
2026-09-18 10:59:19 +0530

    Time: 0 ms (100%), Space: 12.5 MB (31.58%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s, t):
+        result = 0
+
+        for char in s:
+            result ^= ord(char)
+
+        for char in t:
+            result ^= ord(char)
+
+        return chr(result)
